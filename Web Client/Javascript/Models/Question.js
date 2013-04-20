7a6d0157bec78b74c7c4632f35ace3aabbafc0ad
Andy Wise <andrewwise@novasis.co.uk>
2013-04-20 18:15:54 +0100

    Fixes for 1ab5317578fce9ff7ff8c6b61c461d914c7b835b

--- Web Client/Javascript/Models/Question.js
+++ Web Client/Javascript/Models/Question.js
@@ -1,5 +1,7 @@
+var A = ƒ;
 (function(root){
+	'use strict';
 	root.namespace("Models").Question = Backbone.Model.extend({
 		//TODO question properties and methods need adding here
 	});
-})(ƒ);
+})(A);
--- Web Client/Javascript/Models/Quiz.js
+++ Web Client/Javascript/Models/Quiz.js
@@ -1,6 +1,7 @@
+var A = ƒ;
 (function(root){
 	root.namespace("Models").Quiz = Backbone.Collection.extend({
 		model: root.Models.Question
 		//TODO question properties and methods need adding here
 	});
-})(ƒ);
+})(A);
