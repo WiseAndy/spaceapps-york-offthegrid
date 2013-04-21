d4ca14ba82373be54781e5763899a1def146556c
Andy Wise <andrewwise@novasis.co.uk>
2013-04-21 13:29:14 +0100

    fix for db logon

--- Backend/private/connection.php
+++ Backend/private/connection.php
@@ -1,7 +1,7 @@
 <?php
-	$dbname = 'cumcumbr';
-	$dbuser = 'otguser';
-	$dbpass = 'password';
+	$dbname = 'cucumbr';
+	$dbuser = 'root';
+	$dbpass = 'xaVmN1BXKaMqzM3cTFr9';
 	$dbhost = 'localhost';
 	$dbid = mysql_pconnect($dbhost, $dbuser, $dbpass) or die(mysql_error());
 	mysql_select_db($dbname, $dbid);
