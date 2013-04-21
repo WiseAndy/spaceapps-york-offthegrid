0c69d1660ee1cbf12492ab35a437224b7fe5b063
Andy Wise <andrewwise@novasis.co.uk>
2013-04-21 01:04:06 +0100

    View and Template stubbing
    Unknown bug in template stubs that stops namespace being created - may
    need hack for now given time constraints

---
+++ Web Client/Javascript/Views/Quiz.js
@@ -0,0 +1,11 @@
+var A = ƒ;
+(function(root){
+	root.namespace("Models").Quiz = Backbone.View.extend({
+		collection: root.Models.Quiz,
+		template: root.Templates.Quiz,
+		render: function(){
+			this.$el.html(this.template({collection: this.collection}));
+			return this;
+		}
+	});
+})(A);
