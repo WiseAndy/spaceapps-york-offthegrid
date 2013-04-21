adc5bef1a048129f909fe41d628b41ac4a54a170
Andy Wise <andrewwise@novasis.co.uk>
2013-04-21 13:19:15 +0100

    Frontpage integration

--- frontpage/js/main.js
+++ frontpage/js/main.js
@@ -1,8 +1,10 @@
 
 jQuery(document).ready(function($) {
  
-  $(".more").click(function(event){		
+	$(".more").click(function(event){		
 		event.preventDefault();
 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 	});
+
 });
+
